5f2026c2a7fc582be0f19c3b977e0e982a6faca4
Nino Floris <mail@ninofloris.com>
2017-03-20 23:24:37 +0100

    Add new sample2, F# console app with C# library

---
+++ sample2/c1/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    csl1.Say.Hello "F#!"
+    csl1.Say.JsonStuff()
+    0 // return an integer exit code
