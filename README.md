fb8f163a30ccbf23580c312c73cd3ea82888ab06
Nino Floris <mail@ninofloris.com>
2017-03-20 23:24:37 +0100

    Add new sample2, F# console app with C# library

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 ## sample1 - console app with lib
 
-`c1` is a console app, with project reference to `l1`
-`l1` is a lib, who use `Json.NET` package
-vscode is configured to build and run
+`c1` is a console app, with a project reference to `l1`  
+`l1` is a C# lib, that uses the `Json.NET` package  
+vscode is configured to build and run `c1`
+
+## sample2 - console app with C# lib
+
+`c1` is a console app, with a project reference to `csl1`  
+`csl1` is a C# lib, that uses the `Json.NET` package  
+vscode is configured to build and run `c1`
+
 
 good cases:
 
---
+++ sample2/csl1/Say.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace csl1
+{
+    public class Say
+    {
+        public class Movie
+        {
+            public string Name { get; set; }
+            public int Year { get; set; }
+        }
+
+        public static void Hello(string name) => Console.WriteLine($"Hello {name}");
+
+        public static void JsonStuff()
+        {
+            var movies = new[] {
+                new Movie() { Name = "Bad Boys", Year = 1995 },
+                new Movie() { Name = "Bad Boys2", Year = 2003 }
+            };
+            var json = JsonConvert.SerializeObject(movies);
+            Console.WriteLine(json);
+        }
+    }
+}
